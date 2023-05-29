c432c14b2e0b2889d39be83246634a674428a901
Edwin Miguel <edwinmiguelorellana@gmail.com>
2023-05-29 16:22:20 -0500

    animación del hero al entrar a la página.

--- index.js
+++ index.js
@@ -5,9 +5,26 @@ const line3 = document.querySelector('.line:nth-child(3)');
 const header = document.getElementById('nav');
 const navMobileContainer = document.getElementById('navMobile');
 
+window.onload = function () {
+    const heroTitle = document.getElementById('heroTitle');
+    const heroSubtitle = document.getElementById('heroSubtitle');
+    const heroSecondSubtitle = document.getElementById('heroSecondSubtitle');
+    const heroLine1 = document.getElementById('herolineLeft');
+    const heroLine2 = document.getElementById('herolineRight');
+    const heroBtn = document.getElementById('heroBtn');
+
+    heroTitle.classList.add('title-animation');
+    heroSubtitle.classList.add('subtitle-animation');
+    
+    heroLine1.classList.add('line-left-on-load');
+    heroLine2.classList.add('line-right-on-load');
+    
+    heroSecondSubtitle.classList.add('second-subtitle-animation');
+    heroBtn.classList.add('hero-btn--animation');
+
+} 
 
 burgerIcon.addEventListener('click', () => {
     burgerIcon.classList.toggle('active');
     navMobileContainer.classList.toggle('active');
 });
-
