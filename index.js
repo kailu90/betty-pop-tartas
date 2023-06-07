ab9a20d5ba223374038ac37c4f9dd621eac990ea
Edwin Miguel <edwinmiguelorellana@gmail.com>
2023-06-07 16:44:16 -0500

    reefactorizado código js de la opacidad al hacer scroll y agregado borde negro a las líneas del menú mobile

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const line3 = document.querySelector(".line:nth-child(3)");
 const header = document.getElementById("nav");
 const navMobileContainer = document.getElementById("navMobile");
 
+const heroTitleContainer = document.getElementById('heroTitleContainer');
+
 const sectionHero = document.querySelector("#hero");
 const heroTitle = document.getElementById("heroTitle");
 const heroSubtitle = document.getElementById("heroSubtitle");
@@ -17,35 +19,24 @@ const targetElement = document.querySelector("#historia");
 
 let prevScrollPos = window.pageYOffset;
 
-// window.addEventListener("scroll", () => {
-//   let scrollPosition = window.pageYOffset;
+window.addEventListener("scroll", () => {
+  let scrollPosition = window.pageYOffset;
 
-//   if (scrollPosition > prevScrollPos) {
-//     let opacity = 1 - scrollPosition / window.innerHeight;
-//     if (opacity < 0) {
-//       opacity = 0;
-//     }
-//     heroTitle.style.opacity = opacity.toString();
-//     heroSubtitle.style.opacity = opacity.toString();
-//     heroSecondSubtitle.style.opacity = opacity.toString();
-//     heroLine1.style.opacity = opacity.toString();
-//     heroLine2.style.opacity = opacity.toString();
-//     heroBtn.style.opacity = opacity.toString();
-//   } else {
-//     let opacity = 1 - scrollPosition / window.innerHeight;
-//     heroTitle.style.opacity = opacity.toString();
-//     heroSubtitle.style.opacity = opacity.toString();
-//     heroSecondSubtitle.style.opacity = opacity.toString();
-//     heroLine1.style.opacity = opacity.toString();
-//     heroLine2.style.opacity = opacity.toString();
-//     heroBtn.style.opacity = opacity.toString();
-//   }
+  if (scrollPosition > prevScrollPos) {
+    let opacity = 1 - scrollPosition / window.innerHeight;
+    if (opacity < 0) {
+      opacity = 0;
+    }
+    heroTitleContainer.style.opacity = opacity.toString();
+  } else {
+    let opacity = 1 - scrollPosition / window.innerHeight;
+    heroTitleContainer.style.opacity = opacity.toString();
+  }
 
-//   prevScrollPos = scrollPosition;
-// });
+  prevScrollPos = scrollPosition;
+});
 
 const links = document.querySelectorAll('a[href^="#"]');
-
 // Agregar un evento de clic a cada enlace
 links.forEach((link) => {
   link.addEventListener("click", smoothScroll);
@@ -53,7 +44,9 @@ links.forEach((link) => {
 
 // Función para realizar el scroll suave
 function smoothScroll(event) {
-  event.preventDefault(); // Evitar el comportamiento predeterminado del enlace
+  event.preventDefault();
+  burgerIcon.classList.remove('active');
+  navMobileContainer.classList.remove('active');
 
   const targetId = this.getAttribute("href"); // Obtener el ID del elemento de destino
   const targetElement = document.querySelector(targetId); // Obtener el elemento de destino
@@ -66,11 +59,14 @@ function smoothScroll(event) {
   }
 }
 
-
 const main = document.getElementById("main");
-const sectionHistoria = document.getElementById("historia");
 
 burgerIcon.addEventListener("click", () => {
   burgerIcon.classList.toggle("active");
   navMobileContainer.classList.toggle("active");
 });
+
+const burgerLine1 = document.getElementById('#burgerLine1');
+const burgerLine3 = document.getElementById('#burgerLine3');
+
+const backgroundColor = getComputedStyle(targetElement).backgroundColor;
