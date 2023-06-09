eb05f35326c23092fd35f60bc86fb757e60c0e39
Edwin Miguel <edwinmiguelorellana@gmail.com>
2023-06-09 17:27:54 -0500

    primera iteración de los bullets con animación

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const heroLine1 = document.getElementById("herolineLeft");
 const heroLine2 = document.getElementById("herolineRight");
 const heroBtn = document.getElementById("heroBtn");
 
-const targetElement = document.querySelector("#historia");
+const historySection = document.querySelector("#historia");
 
 let prevScrollPos = window.pageYOffset;
 
@@ -34,6 +34,11 @@ window.addEventListener("scroll", () => {
   }
 
   prevScrollPos = scrollPosition;
+
+  if (scrollPosition >= 1) {
+    bulletsAnimation(scrollPosition);
+  }
+
 });
 
 const links = document.querySelectorAll('a[href^="#"]');
@@ -49,11 +54,11 @@ function smoothScroll(event) {
   navMobileContainer.classList.remove('active');
 
   const targetId = this.getAttribute("href"); // Obtener el ID del elemento de destino
-  const targetElement = document.querySelector(targetId); // Obtener el elemento de destino
+  const historySection = document.querySelector(targetId); // Obtener el elemento de destino
 
-  if (targetElement) {
+  if (historySection) {
     window.scrollTo({
-      top: targetElement.offsetTop,
+      top: historySection.offsetTop,
       behavior: "smooth", // Hacer el desplazamiento suave
     });
   }
@@ -69,4 +74,66 @@ burgerIcon.addEventListener("click", () => {
 const burgerLine1 = document.getElementById('#burgerLine1');
 const burgerLine3 = document.getElementById('#burgerLine3');
 
-const backgroundColor = getComputedStyle(targetElement).backgroundColor;
+const backgroundColor = getComputedStyle(historySection).backgroundColor;
+
+
+const productsSection = document.getElementById('productos');
+const footerContainer = document.getElementById('footer');
+
+const circle = document.getElementById('circle');
+const firstBullet = document.getElementById('firstBullet');
+const secondBullet = document.getElementById('secondBullet');
+const thirdBullet = document.getElementById('thirdBullet');
+
+//función para darle animación a los bullets
+function bulletsAnimation(windowPosition) {
+  const heroSectionRect = sectionHero.getBoundingClientRect();
+  const historySectionRect = historySection.getBoundingClientRect();
+  const productsSectionRect = productsSection.getBoundingClientRect();
+  const footerSectionRect = footerContainer.getBoundingClientRect();
+
+
+  if (windowPosition >= heroSectionRect.bottom && windowPosition < historySectionRect.bottom) {
+    firstBullet.style.border = '2px solid var(--fourth-color)';
+    secondBullet.style.border = '2px solid var(--fourth-color)';
+    thirdBullet.style.border = '2px solid var(--fourth-color)';
+  } else if (windowPosition >= historySectionRect.bottom && windowPosition < productsSectionRect.bottom) {
+    firstBullet.style.border = '2px solid var(--third-color)';
+    secondBullet.style.border = '2px solid var(--third-color)';
+    thirdBullet.style.border = '2px solid var(--third-color)';
+  } else if (windowPosition >= productsSectionRect.top) {
+    firstBullet.style.border = '2px solid var(--fourth-color)';
+    secondBullet.style.border = '2px solid var(--fourth-color)';
+    thirdBullet.style.border = '2px solid var(--fourth-color)';
+  } else {
+    firstBullet.style.border = '2px solid var(--third-color)';
+    secondBullet.style.border = '2px solid var(--third-color)';
+    thirdBullet.style.border = '2px solid var(--third-color)';
+  }
+}
+
+
+firstBullet.addEventListener('click', bulletsTransition);
+secondBullet.addEventListener('click', bulletsTransition);
+thirdBullet.addEventListener('click', bulletsTransition);
+
+
+function bulletsTransition() {
+
+  const bulletId = this.getAttribute('id');
+  const circlePosition = circle.getBoundingClientRect();
+
+  if (bulletId === 'secondBullet' && circlePosition.top === 497.5) {
+    circle.style.animation = 'bullet-animation-1 .5s ease-in-out forwards';    
+  } else if (bulletId === 'secondBullet' && circlePosition.top === 553.5) {
+    circle.style.animation = 'bullet-animation-4 .5s ease-in-out forwards';    
+  } else if (bulletId === 'thirdBullet' && circlePosition.top === 525.5) {
+    circle.style.animation = 'bullet-animation-2 .5s ease-in-out forwards';
+  } else if (bulletId === 'thirdBullet' && circlePosition.top === 497.5) {
+    circle.style.animation = 'bullet-animation-3 .5s ease-in-out forwards'; 
+  } else if (bulletId === 'firstBullet' && circlePosition.top === 525.5) {
+    circle.style.animation = 'bullet-animation-5 .5s ease-in-out forwards'; 
+  } else if (bulletId === 'firstBullet' && circlePosition.top === 553.5) {
+    circle.style.animation = 'bullet-animation-6 .5s ease-in-out forwards'; 
+  }
+}
